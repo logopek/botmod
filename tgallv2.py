cd844b3b57c56cfc29c774b352bc571f04abb559
logopek <49311518+logopek@users.noreply.github.com>
2023-06-05 11:33:59 +0300

    Create tgallv2.py

---
+++ tgallv2.py
@@ -0,0 +1,28 @@
+import pyrogram
+
+
+class Module:
+    name = "tag all"
+    short_name = "tgall"
+    activate = "tgall"
+    description = """Tag everyone from chat"""
+
+    @classmethod
+    async def act(cls, client: pyrogram.Client, message: pyrogram.types.Message, *args, **kwargs):
+        all_users = client.get_chat_members(message.chat.id)
+        w = ""
+        x = 0
+        y = message.chat.id
+        await message.delete()
+        async for user in all_users:
+            if x >= 5:
+                await client.send_message(y, w)
+                w = ""
+                x = 0
+            w += '@' + user.user.username + '\n'
+            x += 1
+        await client.send_message(y, w)
+
+    @property
+    async def help(self):
+        return self.description
