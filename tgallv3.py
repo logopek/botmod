ff92d8c99e89e521fafd8d187555e7751ece5fc4
logopek <49311518+logopek@users.noreply.github.com>
2023-06-05 13:56:53 +0300

    Create tgallv3.py

---
+++ tgallv3.py
@@ -0,0 +1,51 @@
+import pyrogram
+
+import asyncio
+
+class Module:
+
+    name = "tag all v3"
+
+    short_name = "tgallw"
+
+    activate = "tgallw"
+
+    description = """Tag everyone from chat"""
+
+    @classmethod
+
+    async def act(cls, client: pyrogram.Client, message: pyrogram.types.Message, *args, **kwargs):
+
+        all_users = client.get_chat_members(message.chat.id)
+
+        w = ""
+
+        x = 0
+
+        y = message.chat.id
+
+        await message.delete()
+
+        async for user in all_users:
+
+            if x >= 5:
+
+                await client.send_message(y, w)
+
+                await asyncio.sleep(0.7)
+
+                w = ""
+
+                x = 0
+
+            w += '@' + user.user.username + '\n'
+
+            x += 1
+
+        await client.send_message(y, w)
+
+    @property
+
+    async def help(self):
+
+        return self.description
